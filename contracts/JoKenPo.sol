af632737c35c3e3db939089837c171a3ecf4f31e
Reshzera <reoshiro13@gmail.com>
2024-03-08 09:40:01 -0300

    feat: jokenpo tests

--- contracts/JoKenPo.sol
+++ contracts/JoKenPo.sol
@@ -8,31 +8,52 @@ contract JoKenPo {
         Paper,
         Scissors
     }
-    address private player1;
-    address payable private immutable owner;
-    Choice private player1Choice;
-    string public result;
-
-    struct Player {
-        address wallet;
-        uint32 wins;
-    }
-
-    Player[] winners;
+    address private _player1;
+    address payable private immutable _owner;
+    Choice private _player1Choice;
+    string private _result;
+    uint256 private _bid = 0.001 ether;
+    uint8 private _commission = 10;
+    mapping(address => uint128) public winners;
 
     constructor() {
-        owner = payable(msg.sender);
+        _owner = payable(msg.sender);
+    }
+
+    function getCommission() external view returns (uint8) {
+        return _commission;
+    }
+
+    function setCommission(uint8 _newCommission) external restricted {
+        require(
+            _player1 == address(0),
+            "You can not change the commission with a game in progress"
+        );
+        _commission = _newCommission;
+    }
+
+    function getBid() external view returns (uint256) {
+        return _bid;
+    }
+
+    function setBid(uint256 _newBid) external restricted {
+        require(
+            _player1 == address(0),
+            "You can not change the bid with a game in progress"
+        );
+        _bid = _newBid;
+    }
+
+    function getResult() external view returns (string memory) {
+        return _result;
     }
 
     function updateWinner(address winner) private {
-        for (uint256 i; i < winners.length; i++) {
-            if (winners[i].wallet == winner) {
-                winners[i].wins++;
-                return;
-            }
+        if (winners[winner] != 0) {
+            winners[winner]++;
+            return;
         }
-
-        winners.push(Player(winner, 1));
+        winners[winner] = 1;
     }
 
     function finishGame(
@@ -40,44 +61,52 @@ contract JoKenPo {
         address winnerAddress
     ) private {
         address contractAddress = address(this);
-        payable(winnerAddress).transfer((contractAddress.balance / 100) * 90);
-        owner.transfer(contractAddress.balance);
+        payable(winnerAddress).transfer(
+            (contractAddress.balance / 100) * (100 - _commission)
+        );
+        _owner.transfer(contractAddress.balance);
 
         updateWinner(winnerAddress);
 
-        result = newResult;
-        player1 = address(0);
-        player1Choice = Choice.None;
+        _result = newResult;
+        _player1 = address(0);
+        _player1Choice = Choice.None;
     }
 
-    function play(Choice _choice) public payable {
-        require(msg.sender != player1, "Player 1 can't play with himself");
+    function play(Choice _choice) external payable {
+        require(msg.sender != _owner, "Owner can not play");
+        require(msg.sender != _player1, "Player 1 can not play with himself");
         require(_choice != Choice.None, "Invalid choice");
-        require(msg.value >= 0.01 ether, "Invalid bid");
+        require(msg.value >= _bid, "Invalid bid");
 
-        if (player1Choice == Choice.None) {
-            player1 = msg.sender;
-            player1Choice = _choice;
-            result = "Player 1 plays waiting player 2";
+        if (_player1Choice == Choice.None) {
+            _player1 = msg.sender;
+            _player1Choice = _choice;
+            _result = "Player 1 plays waiting player 2";
             return;
         }
 
-        if (player1Choice == Choice.Rock && _choice == Choice.Scissors) {
-            finishGame("Player 1 Wins", player1);
+        if (_player1Choice == Choice.Rock && _choice == Choice.Scissors) {
+            finishGame("Player 1 Wins", _player1);
             return;
         }
-        if (player1Choice == Choice.Scissors && _choice == Choice.Paper) {
-            finishGame("Player 1 Wins", player1);
+        if (_player1Choice == Choice.Scissors && _choice == Choice.Paper) {
+            finishGame("Player 1 Wins", _player1);
             return;
         }
-        if (player1Choice == Choice.Paper && _choice == Choice.Rock) {
-            finishGame("Player 1 Wins", player1);
+        if (_player1Choice == Choice.Paper && _choice == Choice.Rock) {
+            finishGame("Player 1 Wins", _player1);
             return;
         }
-        if (player1Choice == _choice) {
-            result = "Draw Game, the prize was doubled";
+        if (_player1Choice == _choice) {
+            _result = "Draw Game, the prize was doubled";
             return;
         }
         finishGame("Player 2 Wins", msg.sender);
+    }
+
+    modifier restricted() {
+        require(msg.sender == _owner, "You do not have permission");
+        _;
     }
 }
--- test/JoKenPo.ts
+++ test/JoKenPo.ts
@@ -6,14 +6,262 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lock", function () {
-  async function deployOneYearLockFixture() {
+enum GameOptions {
+  NONE,
+  ROCK,
+  PAPER,
+  SCISSORS,
+}
+
+const DEFAULT_BID = ethers.parseEther("0.001");
+
+describe("JoKenPo", function () {
+  async function deployFixture() {
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner, player1, player2] = await ethers.getSigners();
 
     const JoKenPoContract = await ethers.getContractFactory("JoKenPo");
     const joKenPo = await JoKenPoContract.deploy();
 
-    return { joKenPo, owner, otherAccount };
+    return { joKenPo, owner, player1, player2 };
   }
+
+  it("Should get commission", async function () {
+    const { joKenPo } = await loadFixture(deployFixture);
+
+    const commission = await joKenPo.getCommission();
+
+    expect(commission).to.equal(10);
+  });
+
+  it("Should set commission", async function () {
+    const { joKenPo } = await loadFixture(deployFixture);
+
+    await joKenPo.setCommission(20);
+
+    const commission = await joKenPo.getCommission();
+
+    expect(commission).to.equal(20);
+  });
+  it("Should NOT set commission (NOT THE OWNER)", async function () {
+    const { joKenPo, player1 } = await loadFixture(deployFixture);
+    const player1Instance = joKenPo.connect(player1);
+
+    await expect(player1Instance.setCommission(20)).to.be.revertedWith(
+      "You do not have permission"
+    );
+  });
+
+  it("Should NOT set commission (GAME IN PROGRESS)", async function () {
+    const { joKenPo, player1 } = await loadFixture(deployFixture);
+
+    const player1Instance = joKenPo.connect(player1);
+
+    await player1Instance.play(GameOptions.ROCK, { value: DEFAULT_BID });
+
+    await expect(joKenPo.setCommission(20)).to.be.revertedWith(
+      "You can not change the commission with a game in progress"
+    );
+  });
+
+  it("Should get bid", async function () {
+    const { joKenPo } = await loadFixture(deployFixture);
+    const initialBid = ethers.parseEther("0.001");
+
+    const bid = await joKenPo.getBid();
+
+    expect(bid).to.equal(initialBid);
+  });
+
+  it("Should set bid", async function () {
+    const { joKenPo } = await loadFixture(deployFixture);
+
+    const newBid = ethers.parseEther("0.002");
+
+    await joKenPo.setBid(newBid);
+
+    const bid = await joKenPo.getBid();
+
+    expect(bid).to.equal(newBid);
+  });
+
+  it("Should NOT set bid (NOT THE OWNER)", async function () {
+    const { joKenPo, player1 } = await loadFixture(deployFixture);
+    const player1Instance = joKenPo.connect(player1);
+
+    await expect(player1Instance.setBid(1)).to.be.revertedWith(
+      "You do not have permission"
+    );
+  });
+
+  it("Should NOT set bid (GAME IN PROGRESS)", async function () {
+    const { joKenPo, player1 } = await loadFixture(deployFixture);
+
+    const player1Instance = joKenPo.connect(player1);
+
+    await player1Instance.play(GameOptions.ROCK, { value: DEFAULT_BID });
+
+    await expect(joKenPo.setBid(1)).to.be.revertedWith(
+      "You can not change the bid with a game in progress"
+    );
+  });
+
+  it("Should play alone", async function () {
+    const { joKenPo, player1 } = await loadFixture(deployFixture);
+    const player1Instance = joKenPo.connect(player1);
+
+    await player1Instance.play(GameOptions.ROCK, {
+      value: DEFAULT_BID,
+    });
+    const result = await joKenPo.getResult();
+
+    expect(result).to.equal("Player 1 plays waiting player 2");
+  });
+
+  it("Should NOT play alone (IS THW OWNER)", async function () {
+    const { joKenPo, owner } = await loadFixture(deployFixture);
+    const ownerInstance = joKenPo.connect(owner);
+
+    await expect(
+      ownerInstance.play(GameOptions.ROCK, { value: DEFAULT_BID })
+    ).to.be.revertedWith("Owner can not play");
+  });
+
+  it("Should NOT play alone (PLAYER 1 PLAYING TWICE)", async function () {
+    const { joKenPo, player1 } = await loadFixture(deployFixture);
+    const player1Instance = joKenPo.connect(player1);
+
+    await player1Instance.play(GameOptions.ROCK, { value: DEFAULT_BID });
+
+    await expect(
+      player1Instance.play(GameOptions.ROCK, { value: DEFAULT_BID })
+    ).to.be.revertedWith("Player 1 can not play with himself");
+  });
+
+  it("Should NOT play alone (INVALID CHOICE)", async function () {
+    const { joKenPo, player1, player2 } = await loadFixture(deployFixture);
+    const player1Instance = joKenPo.connect(player1);
+    const player2Instance = joKenPo.connect(player2);
+
+    await expect(
+      player1Instance.play(GameOptions.NONE, { value: DEFAULT_BID })
+    ).to.be.revertedWith("Invalid choice");
+  });
+
+  it("Should NOT play alone (INVALID BID)", async function () {
+    const { joKenPo, player1 } = await loadFixture(deployFixture);
+    const player1Instance = joKenPo.connect(player1);
+
+    await expect(
+      player1Instance.play(GameOptions.ROCK, { value: 0 })
+    ).to.be.revertedWith("Invalid bid");
+  });
+
+  it("Should play with two players (Draw)", async function () {
+    const { joKenPo, player1, player2 } = await loadFixture(deployFixture);
+    const player1Instance = joKenPo.connect(player1);
+    const player2Instance = joKenPo.connect(player2);
+
+    await player1Instance.play(GameOptions.ROCK, {
+      value: DEFAULT_BID,
+    });
+
+    await player2Instance.play(GameOptions.ROCK, {
+      value: DEFAULT_BID,
+    });
+
+    const result = await joKenPo.getResult();
+
+    expect(result).to.equal("Draw Game, the prize was doubled");
+  });
+
+  it("Should play with two players (Player 1 Wins)", async function () {
+    const { joKenPo, player1, player2 } = await loadFixture(deployFixture);
+    const player1Instance = joKenPo.connect(player1);
+    const player2Instance = joKenPo.connect(player2);
+
+    await player1Instance.play(GameOptions.ROCK, {
+      value: DEFAULT_BID,
+    });
+
+    await player2Instance.play(GameOptions.SCISSORS, {
+      value: DEFAULT_BID,
+    });
+
+    const result = await joKenPo.getResult();
+
+    expect(result).to.equal("Player 1 Wins");
+
+    await player1Instance.play(GameOptions.SCISSORS, {
+      value: DEFAULT_BID,
+    });
+
+    await player2Instance.play(GameOptions.PAPER, {
+      value: DEFAULT_BID,
+    });
+
+    const result2 = await joKenPo.getResult();
+
+    expect(result2).to.equal("Player 1 Wins");
+
+    await player1Instance.play(GameOptions.PAPER, {
+      value: DEFAULT_BID,
+    });
+
+    await player2Instance.play(GameOptions.ROCK, {
+      value: DEFAULT_BID,
+    });
+
+    const result3 = await joKenPo.getResult();
+
+    expect(result3).to.equal("Player 1 Wins");
+  });
+
+  it("Should play with two players (Player 2 Wins)", async function () {
+    const { joKenPo, player1, player2 } = await loadFixture(deployFixture);
+    const player1Instance = joKenPo.connect(player1);
+    const player2Instance = joKenPo.connect(player2);
+
+    await player1Instance.play(GameOptions.ROCK, {
+      value: DEFAULT_BID,
+    });
+
+    await player2Instance.play(GameOptions.PAPER, {
+      value: DEFAULT_BID,
+    });
+
+    const result = await joKenPo.getResult();
+
+    expect(result).to.equal("Player 2 Wins");
+  });
+
+  it("Should increase player 1 wins", async function () {
+    const { joKenPo, player1, player2 } = await loadFixture(deployFixture);
+    const player1Instance = joKenPo.connect(player1);
+    const player2Instance = joKenPo.connect(player2);
+
+    await player1Instance.play(GameOptions.ROCK, {
+      value: DEFAULT_BID,
+    });
+
+    await player2Instance.play(GameOptions.SCISSORS, {
+      value: DEFAULT_BID,
+    });
+    //winners is mappging
+    const player1Wins = await joKenPo.winners(player1.address);
+
+    expect(player1Wins).to.equal(1);
+
+    await player1Instance.play(GameOptions.ROCK, {
+      value: DEFAULT_BID,
+    });
+
+    await player2Instance.play(GameOptions.SCISSORS, {
+      value: DEFAULT_BID,
+    });
+
+    const player1Wins2 = await joKenPo.winners(player1.address);
+
+    expect(player1Wins2).to.equal(2);
+  });
 });
