d1282089bfc221e69603fa83927da01656064af3
Reshzera <reoshiro13@gmail.com>
2024-03-09 15:41:58 -0300

    feat: add deploy to the contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,10 +2,18 @@ import { ethers } from "hardhat";
 
 async function main() {
   const joKenPoContract = await ethers.deployContract("JoKenPo");
-
   await joKenPoContract.waitForDeployment();
-
   console.log("JoKenPo deployed to:", await joKenPoContract.getAddress());
+  const jkpAdapterContract = await ethers.deployContract("JKPAdapter");
+  await jkpAdapterContract.waitForDeployment();
+  console.log("JKPAdapter deployed to:", await jkpAdapterContract.getAddress());
+  await jkpAdapterContract.upgradeImplementation(
+    await joKenPoContract.getAddress()
+  );
+  console.log(
+    "JoKenPo implementation upgraded to:",
+    await joKenPoContract.getAddress()
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
